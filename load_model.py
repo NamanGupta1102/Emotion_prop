ce22a158d1059cb077f1a242fe13c5795a1e9e31
NamanGupta1102 <naman1102@gmail.com>
2025-04-10 11:40:37 -0400

    back to old

--- load_model.py
+++ load_model.py
@@ -6,9 +6,9 @@ os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 from transformers import pipeline
 
 if __name__ == "__main__":
-    classifier = pipeline("sentiment-analysis", model="michellejieli/emotion_text_classifier")
+    classifier = pipeline("sentiment-analysis", model="michellejieli/emotion_text_classifier", custom_cache_dir="./cache")
     # Your other code here...
-    print(classifier("You suck "))
+    print(classifier("You are good "))
     with open('model.pkl', 'wb') as f:
         pickle.dump(classifier, f)
     
